c8b491c5d819ae3a6fd8e19f2601046ee0749fcc
Yishen Miao <mys721tx@gmail.com>
2022-03-27 00:27:59 -0700

    Refactoring ac module
    Refactors ac module.

--- src/parser/ac.rs
+++ src/parser/ac.rs
@@ -1,12 +1,10 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::{char, digit1, newline, satisfy, space0, space1};
-use nom::combinator::{map_res, opt, recognize, value};
-use nom::error::ParseError;
+use nom::character::complete::{newline, satisfy, space0, space1};
+use nom::combinator::{opt, recognize};
 use nom::multi::{many1, many_m_n};
-use nom::sequence::{delimited, preceded, terminated, tuple};
+use nom::sequence::{delimited, tuple};
 use nom::IResult;
-use nom::{AsChar, InputIter, InputLength, Slice};
 
 fn accession(i: &str) -> IResult<&str, &str> {
     alt((
@@ -36,12 +34,10 @@ fn accession(i: &str) -> IResult<&str, &str> {
 }
 
 pub fn ac_line(i: &str) -> IResult<&str, Vec<&str>> {
-    let (l, r) = many1(preceded(
+    let (l, r) = many1(delimited(
         tuple((tag("AC"), space1)),
-        terminated(
-            many_m_n(1, 8, delimited(space0, accession, tag(";"))),
-            newline,
-        ),
+        many_m_n(1, 8, delimited(space0, accession, tag(";"))),
+        newline,
     ))(i)?;
 
     Ok((l, r.concat()))
@@ -92,17 +88,15 @@ AC   Q99605;
                 )
             ))
         );
-        assert!(
-            ac_line(
-                "AC   A0AAA000000;
+        assert!(ac_line(
+            "AC   A0AAA000000;
 "
-            ).is_err()
-        );
-        assert!(
-            ac_line(
-                "AC   A0AAA0A000A;
+        )
+        .is_err());
+        assert!(ac_line(
+            "AC   A0AAA0A000A;
 "
-            ).is_err()
-        );
+        )
+        .is_err());
     }
 }
