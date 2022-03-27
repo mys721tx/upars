62ea09a5926662f5e255f67ab3b61e2eecd71f92
Yishen Miao <mys721tx@gmail.com>
2022-03-27 00:17:24 -0700

    Implementing AC parser
    Implements AC parser.

--- src/parser/ac.rs
+++ src/parser/ac.rs
@@ -1,10 +1,10 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::{char, digit1, satisfy, space1};
+use nom::character::complete::{char, digit1, newline, satisfy, space0, space1};
 use nom::combinator::{map_res, opt, recognize, value};
 use nom::error::ParseError;
-use nom::multi::many_m_n;
-use nom::sequence::{terminated, tuple};
+use nom::multi::{many1, many_m_n};
+use nom::sequence::{delimited, preceded, terminated, tuple};
 use nom::IResult;
 use nom::{AsChar, InputIter, InputLength, Slice};
 
@@ -35,6 +35,18 @@ fn accession(i: &str) -> IResult<&str, &str> {
     ))(i)
 }
 
+pub fn ac_line(i: &str) -> IResult<&str, Vec<&str>> {
+    let (l, r) = many1(preceded(
+        tuple((tag("AC"), space1)),
+        terminated(
+            many_m_n(1, 8, delimited(space0, accession, tag(";"))),
+            newline,
+        ),
+    ))(i)?;
+
+    Ok((l, r.concat()))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,5 +65,32 @@ mod tests {
         // assert!(accession("A0AAA000000").is_err());
         // assert!(accession("A0AAA0A000A").is_err());
         assert!(accession("O0AAAA").is_err());
+    }
+
+    #[test]
+    fn ac_line_test() {
+        assert_eq!(
+            ac_line(
+                "AC   P00321;
+"
+            ),
+            Ok(("", vec!("P00321")))
+        );
+        assert_eq!(
+            ac_line(
+                "AC   Q16653; O00713; O00714; O00715; Q13054; Q13055; Q14855; Q92891;
+AC   Q92892; Q92893; Q92894; Q92895; Q93053; Q96KU9; Q96KV0; Q96KV1;
+AC   Q99605;
+"
+            ),
+            Ok((
+                "",
+                vec!(
+                    "Q16653", "O00713", "O00714", "O00715", "Q13054", "Q13055", "Q14855", "Q92891",
+                    "Q92892", "Q92893", "Q92894", "Q92895", "Q93053", "Q96KU9", "Q96KV0", "Q96KV1",
+                    "Q99605"
+                )
+            ))
+        );
     }
 }
